6e29809085fcbaac9434987d737d4c87902185d4
cbloodsworth <christopherbloodsworth@gmail.com>
2022-11-16 19:28:29 -0500

    Added React and Next.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import fetch from "isomorphic-unfetch"
+
+
+
+export default class extends Component {
+    static async getInitialProps() {
+        const res = await fetch("https://random.dog/woof.json?filter=mp4,webm")
+        const data = await res.json()
+      
+        return {
+            imageURL: data.url
+        }
+    }
+    render () {
+        return (
+            <div className="homepage-wrapper">
+                <h1>Random dog!</h1>
+                <img src={this.props.imageURL} width="300" />
+            </div>
+        )
+    }
+}
