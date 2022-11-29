575255d98c124d2a1983e5346710b82aafb779fd
cbloodsworth <christopherbloodsworth@gmail.com>
2022-11-28 19:43:20 -0500

    Made slight changes to the index.js image size.

--- .next/static/webpack/webpack.453c8a97688d97cf.hot-update.js
+++ .next/static/webpack/webpack.453c8a97688d97cf.hot-update.js
@@ -11,21 +11,8 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bfd82aa31d5b536a"; }
+/******/ 	__webpack_require__.h = function() { return "e435dbf2f71fbf79"; }
 /******/ }();
-/******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ 
-/******/ // noop fns to prevent runtime errors during initialization
-/******/ if (typeof self !== "undefined") {
-/******/ 	self.$RefreshReg$ = function () {};
-/******/ 	self.$RefreshSig$ = function () {
-/******/ 		return function (type) {
-/******/ 			return type;
-/******/ 		};
-/******/ 	};
-/******/ }
 /******/ 
 /******/ }
 );
