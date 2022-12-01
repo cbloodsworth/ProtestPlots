a9926a5c309731222db5c5e332fa957c9b9c9317
cbloodsworth <christopherbloodsworth@gmail.com>
2022-11-30 19:42:55 -0500

    Switching repository structure

---
+++ src/components/sidebar/sidebar-section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './sidebar'
+import './sidebar-section.css'
+
+const SideBarSection = () => (
+  <>
+    <div className="sidebar-section">
+      <Sidebar />
+    </div>
+  </>
+)
+
+export default SideBarSection
